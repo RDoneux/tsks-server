535bbc57b7b35a806ff0e4245343d4b8f5a4ffbb
Rob Doneux <robertdoneux@gmail.com>
2024-10-24 21:06:51 +0100

    adds jest-junit reporters

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  reporters: [
+    "default",
+    "jest-junit"
+  ],
   testEnvironment: 'node',
   transform: {
     '^.+.tsx?$': ['ts-jest', {}],
